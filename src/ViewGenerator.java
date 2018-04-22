bcb97247c27b29df599b2b1ad6901e9c60dbba6b
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2018-04-22 09:37:40 -0700

    cleaned up code

--- src/ViewGenerator.java
+++ src/ViewGenerator.java
@@ -1,8 +1,8 @@
 public class ViewGenerator
 {
     public static int progress;
-    public static String version = "1.1.0";
-    
+    public static final String VERSION = "1.1.0";
+
     public static void main (String [] args) throws Exception
     {
         Referers.initialize();
@@ -18,5 +18,22 @@ public class ViewGenerator
 
             }
         }
+    }
+
+    public static String linkString(String site)
+    {
+        if (!site.substring(0, 7).equals("http://"))
+        {
+            if (site.substring(0,8).equals("https://"))
+            {
+                site = site.substring(8);
+                site = "http://" + site;
+            }
+            else
+            {
+                site = "http://" + site;
+            }
+        }
+        return site;
     }
 }
