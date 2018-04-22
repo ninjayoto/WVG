b79266895a3664c06579d8f28f2f2aa10856f6ba
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2018-04-21 18:13:50 -0700

    added version

--- src/ViewGenerator.java
+++ src/ViewGenerator.java
@@ -1,6 +1,7 @@
 public class ViewGenerator
 {
     public static int progress;
+    public static String version = "1.1.0";
     
     public static void main (String [] args) throws Exception
     {
