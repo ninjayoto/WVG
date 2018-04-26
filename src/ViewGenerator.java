d724a7ae1257ac9213ee8e89720ccddc5102a3e4
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2018-04-25 19:52:58 -0700

    helper methods for update

--- src/ViewGenerator.java
+++ src/ViewGenerator.java
@@ -1,3 +1,8 @@
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ViewGenerator
 {
     public static int progress;
@@ -14,7 +19,6 @@ public class ViewGenerator
             {
                 Cli cli = new Cli(args);
                 cli.parse();
-
             }
         }
     }
@@ -34,5 +38,47 @@ public class ViewGenerator
             }
         }
         return site;
+    }
+
+    public static boolean isUpdate()
+    {
+        try
+        {
+            Proxies.newProxy();
+            System.getProperties().put( "proxySet", "true" );
+            System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+            System.getProperties().put( "socksProxyPort", "9050" );
+            System.setProperty("socksProxyVersion" , "5");
+            URL url = new URL("https://raw.githubusercontent.com/BitsByWill/WVG/master/src/CURRENT_VER");
+            URLConnection con = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line = "";
+            String newVersion = "";
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                newVersion += line;
+            }
+            Proxies.stopProxy();
+            if (!newVersion.equals(VERSION))
+            {
+                return true;
+            }
+        }
+        catch (Exception e)
+        {
+        }
+        return false;
+    }
+
+    public static void update(boolean windows) throws Exception
+    {
+        if (windows)
+        {
+            Process p = Runtime.getRuntime().exec("resources\\wintor\\check.exe");
+        }
+        else
+        {
+            Process p = Runtime.getRuntime().exec("resources/linuxtor/check");
+        }
     }
 }
