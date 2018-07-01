7bea2d2401430f6accfb076e0ec0143bca121de0
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2018-06-30 23:08:50 -0700

    added queue system for multithreaded spider

---
+++ src/LinkQueue.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class LinkQueue
+{
+    private LinkedList<String> links = new LinkedList<String>();
+    
+    public void enqueueLink(String url)
+    {
+        links.add(url);
+    }
+    
+    public String dequeueLink()
+    {
+        return links.remove(0);
+    }
+    
+    public int getLength()
+    {
+        return links.size();
+    }
+}
